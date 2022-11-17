fc0e895420b4a511fe6ac5404f7b3936d5134287
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-11-17 19:45:06 +0600

    code changed

--- Automation.X/remote.c
+++ Automation.X/remote.c
@@ -1,5 +1,6 @@
-
 #include <xc.h>
+
+#include<pic18f2520.h>
 
 #define _XTAL_FREQ  20000000
 
@@ -7,7 +8,62 @@
 
 #include<stdlib.h>
 
-#include "config.h"
+
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = ON        // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bits (Brown-out Reset enabled and controlled by software (SBOREN is enabled))
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
 
 
 #define tric PORTBbits.RB1
@@ -30,8 +86,6 @@
 
 #define rly3LED PORTBbits.RB5
 
-#define rly5LED PORTBbits.RB6
-
 #define rly4LED PORTCbits.RC1
 
 #define fanLED PORTCbits.RC0
@@ -51,7 +105,7 @@ char result[10];
 
 int j=0;
 
-   
+
 
 void delay(int time)
 
@@ -69,11 +123,11 @@ void timer()           // 10 -> 1us
 
 {
 
-   T0CONbits.T0PS0=0; 
+   T0PS0=0;
 
-   T0CONbits.T0PS1=0;
+   T0PS1=0;
 
-   T0CONbits.T0PS2=0;
+   T0PS2=0;
 
    PSA=0;      //Timer Clock Source is from Prescaler
 
@@ -92,27 +146,27 @@ void timer()           // 10 -> 1us
 }
 
 
-void main(void) 
+void main(void)
 
 {
 
    ADCON1=0b00001111;
 
-   TRISBbits.TRISB1=0;
+   TRISB1=0;
 
-   TRISBbits.TRISB2=1;
+   TRISB2=1;
 
-   TRISBbits.TRISB3=0;
+   TRISB3=0;
 
-   TRISBbits.TRISB4=0;
+   TRISB4=0;
 
-   TRISBbits.TRISB5=0;
+   TRISB5=0;
 
    TRISC=0x00;
 
    TRISA=0x00;
 
-   PORTA=0xc0;             
+   PORTA=0xc0;
 
    TRISB6=0;
 
@@ -146,7 +200,7 @@ void main(void)
 
     INTEDG0 = 0; // Interrupt on falling edge
 
-    INT0IE = 1; // Enable the INT0 external interrupt (RB0) 
+    INT0IE = 1; // Enable the INT0 external interrupt (RB0)
 
     INT0IF = 0; // Clears INT0 External Interrupt Flag bit
 
@@ -157,7 +211,7 @@ void main(void)
 
    while(1)
 
-   {   
+   {
 
        while(ir == 1);
 
@@ -173,7 +227,7 @@ void main(void)
 
        dat[i]=TMR0;
 
-       
+
 
        if(dat[1] > 5000 && dat[1]<12000)
 
@@ -196,26 +250,27 @@ void main(void)
 
        {
 
-            GIE=0;
+        GIE=0;
 
-            delay(50);
+        delay(50);
 
-            cmd=0;
+        cmd=0;
 
-            for(j=26;j<34;j++)
+        for(j=26;j<34;j++)
 
         {
 
-            if(dat[j]>1000 && dat[j]<2000){
+            if(dat[j]>1000 && dat[j]<2000)
 
                 cmd<<=1;
-            }
+
 
             else if(dat[j]>3500 && dat[j]<4500)
 
             {
 
                 cmd|=0x01;
+
                 cmd<<=1;
 
             }
@@ -224,130 +279,196 @@ void main(void)
 
         cmd>>=1;
 
-         
 
-         if(cmd == 0xAF)
+
+         if(cmd == 0xE3)
+
          {
+
              relay1=~relay1;
+
              rly1LED=~rly1LED;
+
          }
 
-         
 
-         else if(cmd == 0x27)
+
+         else if(cmd == 0xE7)
+
          {
+
              relay2=~relay2;
+
              rly2LED=~rly2LED;
+
          }
 
-         
 
-         else if(cmd == 0x07)
+
+         else if(cmd == 0xE3)
+
          {
+
              relay3=~relay3;
+
              rly3LED=~rly3LED;
+
          }
 
-         
 
-         else if(cmd == 0xCF)
+
+         else if(cmd == 0xE3)
+
          {
+
              relay4=~relay4;
+
              rly4LED=~rly4LED;
+
          }
 
-         
 
-         else if(cmd == 0x5f)
+
+         else if(cmd == 0xF6)
+
          {
+
              speed++;
 
              if(speed>5)
+
              {
-                 speed=5; 
+
+                 speed=5;
+
              }
+
          }
 
-         
 
-         else if(cmd == 0x9f)
+
+         else if(cmd == 0xEA)
+
          {
+
              speed--;
 
              if(speed<=0)
+
              {
-                 speed=0; 
+
+                 speed=0;
+
              }
+
          }
 
-         
+
 
         if(speed == 5)        // turned off  5x2= 10ms triger    //speed 0
+
         {
+
             PORTA=0xC0;           // display 0
+
             PORTBbits.RB6=1;
-            fanLED=0;             
+
+            fanLED=0;
+
         }
 
-        
+
 
         else if(speed == 4 )    //  8 ms trigger    //speed 1
-        { 
+
+        {
+
             PORTA=0xfc;        // displaying 1
+
             PORTBbits.RB6=1;
+
             fanLED=1;
+
         }
 
-        
+
 
         else if(speed == 3)   //   6 ms trigger     // speed 2
+
         {
-            PORTA=0xE4;       // displaying 2                    
+
+            PORTA=0xE4;       // displaying 2
+
             PORTBbits.RB6=0;
+
             fanLED=1;
+
         }
 
-        
+
 
         else if(speed == 2)   // 4ms trigger  // speed 3
+
         {
+
             PORTA=0xF0;       // displaying 3
+
             PORTBbits.RB6=0;
+
             fanLED=1;
+
         }
 
-        
+
 
         else if(speed == 1)    // 2ms trigger  // speed 4
+
         {
+
             PORTA=0xD9;          // displaying 4
+
             PORTBbits.RB6=0;
+
             fanLED=1;
+
         }
 
-        
+
 
         else if(speed == 0)        // 0ms trigger  // speed 5  full power
+
         {
+
            PORTA=0xD2;            // displaying 5
+
            PORTBbits.RB6=0;
+
            fanLED=1;
+
         }
 
-        
+
 
         else
+
         {
+
             PORTBbits.RB6=1;
+
             PORTA=0xff;         // display off
+
             fanLED=0;
+
         }
 
-        
+
 
         i=0;
+
         INT0IE = 1;
+
         GIE=1;
+
        }
 
 }
@@ -355,12 +476,15 @@ void main(void)
 }
 
 
-void __interrupt() isr(void)
+void interrupt isr()
+
 {
+
     if(INT0IF)
 
     {
-        delay(speed);
+
+        //delay(speed);
 
         tric=1;
 
@@ -372,12 +496,17 @@ void __interrupt() isr(void)
 
     }
 
-    
 
-   if(TMR0IF)  //Check if it is TMR0 Overflow ISR   
+
+   if(TMR0IF)  //Check if it is TMR0 Overflow ISR
+
    {
+
       TMR0IF=0;
-   } 
+
+   }
+
+
 
 }
 
